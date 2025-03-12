4bb3c253b3b052f013390b476ad6e2d1634c372a
fiez alhag <148772477+FiezAlhagwork@users.noreply.github.com>
2025-03-12 15:06:21 +0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -100,7 +100,7 @@ export default function Home() {
         ) : (
           <div className="home__error-container">
             <h2 className="text-black text-xl font-bold">Oops , no results</h2>
-            <p>{allCars.error}</p>
+            {/* <p>{allCars.error}</p> */}
           </div>
         )}
       </div>
